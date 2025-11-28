5d2bfafb4283199f1edcc925b7372e8bd79a8448
Michael Liu <microphoneale@hotmail.com>
2025-11-28 00:18:34 -0500

    Now prints resources and criterion at the beginning of turn

--- modules/Game.cc
+++ modules/Game.cc
@@ -1,54 +1,101 @@
 module Game;
 
 import <iostream>;
+import <array>;
 import Player;
 import Board;
 import RandomGenerator;
+import IDiceStrategy;
 import FairDiceStrategy;
 import LoadedDiceStrategy;
+import WatanTypes;
 
 using namespace std;
 
 Game::Game()
     : quit(false),
     currentTurn(1),
+    currentPlayer(0), // Blue starts
     board(),
-    player(PlayerColour::Blue),
+    players{ Player(PlayerColour::Blue),
+             Player(PlayerColour::Red),
+             Player(PlayerColour::Orange),
+             Player(PlayerColour::Yellow) },
     rng(123),
     dice(make_unique<FairDiceStrategy>())
 {
+    // Initial banner when the game starts
     startTurnMessage();
 }
 
-bool Game::isRunning() const { return !quit; }
-void Game::requestQuit() { quit = true; }
+bool Game::isRunning() const {
+    return !quit;
+}
 
-Board& Game::getBoard() { return board; }
-Player& Game::getPlayer() { return player; }
+void Game::requestQuit() {
+    quit = true;
+}
+
+Board& Game::getBoard() {
+    return board;
+}
+
+Player& Game::getPlayer() {
+    return players[currentPlayer];
+}
+
+const Player& Game::getPlayer() const {
+    return players[currentPlayer];
+}
+
+Player& Game::getPlayer(int index) {
+    return players.at(index);
+}
+
+const Player& Game::getPlayer(int index) const {
+    return players.at(index);
+}
+
+int Game::getCurrentPlayerIndex() const {
+    return currentPlayer;
+}
+
+PlayerColour Game::getCurrentPlayerColour() const {
+    return players[currentPlayer].getColour();
+}
 
 void Game::startTurnMessage() {
-    cout << "===== TURN " << currentTurn << " =====\n";
-    cout << "Player " << toString(player.getColour()) << ", it is your turn.\n";
+    cout << "----------------------------------------\n";
+    cout << "Student " << toString(getCurrentPlayerColour()) << "'s turn.\n";
+    // Show the current player's status at the start of their turn
+    getPlayer().printStatus(cout);
 }
 
 void Game::nextTurn() {
-    currentTurn++;
+    // Move to next player in order: Blue, Red, Orange, Yellow
+    currentPlayer = (currentPlayer + 1) % 4;
+    ++currentTurn;
     startTurnMessage();
 }
 
 int Game::rollDice() {
     int val = dice->roll(rng);
-    cout << "You rolled: " << val << "\n";
+    cout << "Student " << toString(getCurrentPlayerColour())
+        << " rolled " << val << ".\n";
+
+    // Still using simple distribution for now (full rules in later versions)
     board.distributeResources(val);
     return val;
 }
 
 void Game::setDiceFair() {
     dice = make_unique<FairDiceStrategy>();
-    cout << "Dice set to FAIR.\n";
+    cout << "Dice set to FAIR for student "
+        << toString(getCurrentPlayerColour()) << ".\n";
 }
 
 void Game::setDiceLoaded() {
     dice = make_unique<LoadedDiceStrategy>();
-    cout << "Dice set to LOADED.\n";
+    cout << "Dice set to LOADED for student "
+        << toString(getCurrentPlayerColour()) << ".\n";
 }
