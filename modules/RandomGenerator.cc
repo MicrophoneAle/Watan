0cf06053e0f48729f49b38e982f660ff03acfda2
Michael Liu <microphoneale@hotmail.com>
2025-11-27 20:39:41 -0500

    Added more comments

--- modules/RandomGenerator.cc
+++ modules/RandomGenerator.cc
@@ -5,9 +5,13 @@ using namespace std;
 
 export class RandomGenerator {
 public:
+    // Generates and random seed
     RandomGenerator() : engine(random_device{}()) {}
+
+    // Generates based on a specific seed for the command -seed
     RandomGenerator(unsigned int seed) : engine(seed) {}
 
+    // Generates a random number in the range [low, high]
     int intInRange(int low, int high) {
         uniform_int_distribution<int> dist(low, high);
         return dist(engine);
